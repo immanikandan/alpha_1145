37ec8fa41b64803940c094f76c002a02ad772cab
immanikandan <immanikandan>
2021-01-25 13:22:49 +0000

    updated generated code

--- application/services/custom_services/alpha1/src/models/zee.ts
+++ application/services/custom_services/alpha1/src/models/zee.ts
@@ -5,7 +5,8 @@ const Schema = mongoose.Schema;
 
 export const zeeSchema = new Schema({
    titile: String,
-   description: String
+   description: String,
+   name: String
 })
 
 const zeeModel = mongoose.model('zee', zeeSchema, 'zee');
